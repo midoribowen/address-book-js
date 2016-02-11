53b5b1dadf00a84e21a7ea34c394761113804009
Midori Bowen <midori.p.bowen@gmail.com>
2016-02-10 18:28:28 -0800

    Show a contact if clicked from all contacts list.

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,26 @@ function Contact(firstName, lastName) {
 Contact.prototype.fullName = function (firstName, lastName) {
   return this.firstName + " " + this.lastName;
 };
+
+$(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var firstNameInput = $("input#new-first-name").val();
+    var lastNameInput = $("input#new-last-name").val();
+    var newContact = new Contact(firstNameInput, lastNameInput);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h3").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+   });
+});
